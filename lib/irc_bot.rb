34142c8d782e0562440c66b0572da439f613cc54
sdqali <sadiqalikm@gmail.com>
2010-02-24 03:44:27 +0530

    Added all basic capabilities

--- lib/irc_bot.rb
+++ lib/irc_bot.rb
@@ -1,10 +1,29 @@
 class IrcBot
-  def initialize server, port, channel, user_name, nick
-    @server = server
-    @port  = port
+  def initialize server, port
+    @socket = TCPSocket.open server, port
+  end
+  
+  def post message
+    @socket.put message
   end
 
-  def open_connection
-    TCPSocket.open(@server, @port)
+  def set_nick nick
+    post "NICK #{nick}"
+  end
+
+  def set_user user_name, full_name
+    post "USER #{user_name} 0 * #{full_name}"
+  end
+
+  def join_channel channel
+    post "JOIN #{channel}"
+  end
+
+  def post_message channel, message
+    post "PRIVMSG #{channel} :#{message}"
+  end
+
+  def quit
+    post "QUIT"
   end
 end
--- spec/irc_bot_spec.rb
+++ spec/irc_bot_spec.rb
@@ -1,13 +1,72 @@
 require File.dirname(__FILE__) + '/../lib/irc_bot'
 
 describe IrcBot do
-  it "should open a TCP socket at specified server when connection opened" do
+  it "should open a TCP socket at specified server when initialized" do
     TCPSocket.should_receive(:open).with("server", anything())
-    IrcBot.new("server", nil, nil, nil, nil).open_connection
+    IrcBot.new "server", nil
   end
 
-  it "should open a TCP socket at specified port when connection opened" do
+  it "should open a TCP socket at specified port when initialized" do
     TCPSocket.should_receive(:open).with(anything(), "port")
-    IrcBot.new(nil, "port", nil, nil, nil).open_connection
+    IrcBot.new nil, "port"
+  end
+
+  context "when posting messages" do
+    it "should put all messages to the socket" do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      mock_socket.should_receive(:put).with("message")
+      IrcBot.new(nil, nil).post "message"
+    end
+  end
+
+  context "when setting nick" do
+    it "should post nick_name with NICK to the server" do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      bot = IrcBot.new(nil, nil)
+      bot.should_receive(:post).with("NICK nick")
+      bot.set_nick "nick"
+    end
+  end
+
+  context "when setting user" do
+    it "should post username and FullName with USER to the server" do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      bot = IrcBot.new(nil, nil)
+      bot.should_receive(:post).with("USER username 0 * FullName")
+      bot.set_user "username", "FullName"
+    end
+  end
+
+  context "when joining a channel" do
+    it "should post the channel name with JOIN to the server " do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      bot = IrcBot.new(nil, nil)
+      bot.should_receive(:post).with("JOIN #channel")
+      bot.join_channel "#channel"
+    end
+  end
+
+  context "when sending a message" do
+    it "should post the message with PRIVMSG and channel name" do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      bot = IrcBot.new(nil, nil)
+      bot.should_receive(:post).with("PRIVMSG #channel :message")
+      bot.post_message "#channel", "message"
+    end
+  end
+
+  context "when quitting" do
+    it "should post QUIT to the server" do
+      mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(mock_socket)
+      bot = IrcBot.new(nil, nil)
+      bot.should_receive(:post).with("QUIT")
+      bot.quit
+    end
   end
 end
