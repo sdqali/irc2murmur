9423b56ff24e646c2df343ae26cd526b4732b04c
sdqali <sadiqalikm@gmail.com>
2010-03-03 23:37:45 +0530

    Refactored specs into contexts and describes

--- spec/irc_bot_spec.rb
+++ spec/irc_bot_spec.rb
@@ -22,63 +22,41 @@ describe IrcBot do
     end
   end
 
-  context "when setting nick" do
-    it "should post nick_name with NICK to the server" do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("NICK nick")
-      bot.set_nick "nick"
+  describe "sending IRC messages to the server" do
+    before(:each) do
+      @mock_socket = mock(TCPSocket)
+      TCPSocket.should_receive(:open).and_return(@mock_socket)
+      @bot = IrcBot.new(nil, nil)
     end
-  end
 
-  context "when setting user" do
-    it "should post username and FullName with USER to the server" do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("USER username 0 * FullName")
-      bot.set_user "username", "FullName"
+    it "should post nick_name with NICK to the server when setting nick" do
+      @bot.should_receive(:post).with("NICK nick")
+      @bot.set_nick "nick"
     end
-  end
 
-  context "when joining a channel" do
-    it "should post the channel name with JOIN to the server " do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("JOIN #channel")
-      bot.join_channel "#channel"
+    it "should post username and FullName with USER to the server when setting user" do
+      @bot.should_receive(:post).with("USER username 0 * FullName")
+      @bot.set_user "username", "FullName"
     end
-  end
 
-  context "when sending a message" do
-    it "should post the message with PRIVMSG and channel name" do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("PRIVMSG #channel :message")
-      bot.post_message "#channel", "message"
+    it "should post the channel name with JOIN to the server when joining channel" do
+      @bot.should_receive(:post).with("JOIN #channel")
+      @bot.join_channel "#channel"
     end
-  end
 
-  context "when quitting" do
-    it "should post QUIT to the server" do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("QUIT")
-      bot.quit
+    it "should post the message with PRIVMSG and channel name when poting to a channel" do
+      @bot.should_receive(:post).with("PRIVMSG #channel :message")
+      @bot.post_message "#channel", "message"
     end
-  end
 
-  context "when processing incoming messages" do
+    it "should post QUIT to the server when quitting" do
+      @bot.should_receive(:post).with("QUIT")
+      @bot.quit
+    end
+
     it "should post a PONG to the source if the message is a PING" do
-      mock_socket = mock(TCPSocket)
-      TCPSocket.should_receive(:open).and_return(mock_socket)
-      bot = IrcBot.new(nil, nil)
-      bot.should_receive(:post).with("PONG sdqali")
-      bot.process "PING :sdqali"
+      @bot.should_receive(:post).with("PONG sdqali")
+      @bot.process "PING :sdqali"
     end
   end
 end
