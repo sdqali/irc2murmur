ea069e818f2317616ad00072956a5336e7af9830
sdqali <sadiqalikm@gmail.com>
2010-03-04 01:46:52 +0530

    Added two debugging put statements

--- irc2murmur.rb
+++ irc2murmur.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/lib/irc_bot'
 bot= IrcBot.new "irc.freenode.net", "6667"
+puts "Successfully created the bot......."
 bot.set_nick "foobot"
 bot.set_user "foobot", "Foo Bot"
 bot.join_channel "#lisp"
 obs = IrcObserver.new bot
+puts "irc2murmur now listening to the conversations....."
 bot.run
