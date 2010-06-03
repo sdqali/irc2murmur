0b3c05b639c444587901410c0334f59c7f75c9a8
sdqali <sadiqalikm@gmail.com>
2010-06-03 16:09:50 +0530

    Split the specs for Mingle#post_murmur into better specs

--- spec/mingle_spec.rb
+++ spec/mingle_spec.rb
@@ -10,9 +10,23 @@ describe Mingle do
     Mingle.new(http_client, "host", 8080, "test_project", "user", "password")
   end
 
-  it "should post murmur to mingle server" do
-    http_client = StubHttpClient.new
-    http_client.should_receive(:post).with("http://host:8080/api/v2/projects/test_project/murmurs.xml", "murmur[body]" => "foo")
-    Mingle.new(http_client, "host", 8080, "test_project", "user", "password").post_murmur("foo")
+  context "posting murmurs" do
+    it "post to the correct mingle server" do
+      http_client = StubHttpClient.new
+      http_client.should_receive(:post).with(%r{.*://host:8080/.*}, anything)
+      Mingle.new(http_client, "host", 8080, nil, nil, nil).post_murmur(nil)
+    end
+
+    it "should posts to the correct url" do
+      http_client = StubHttpClient.new
+      http_client.should_receive(:post).with(%r{.*/api/v2/projects/the-project/murmurs.xml}, anything)
+      Mingle.new(http_client, nil, nil, "the-project", nil, nil).post_murmur(nil)
+    end
+
+    it "posts in the correct format" do
+      http_client = StubHttpClient.new
+      http_client.should_receive(:post).with(anything, "murmur[body]" => "foo")
+      Mingle.new(http_client, nil, nil, nil, nil, nil).post_murmur("foo")
+    end
   end
 end
