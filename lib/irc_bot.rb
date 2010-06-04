729c35e5d877ab0482b7ce6415306be639452ab0
sdqali <sadiqalikm@gmail.com>
2010-06-04 16:09:54 +0530

    Reordered specs in irc_bot_spec

--- lib/irc_bot.rb
+++ lib/irc_bot.rb
@@ -20,14 +20,21 @@ module Irc2Murmur
     def set_user user_name, full_name
       post "USER #{user_name} 0 * #{full_name}"
     end
-    
-    def post message
-      @socket.puts message
-    end
 
     def join_channel channel
       @channel = "##{channel}"
       post "JOIN #{@channel}"
+    end
+
+    def run
+      until @socket.eof? do
+        msg = @socket.gets
+        process msg
+      end
+    end
+
+    def post message
+      @socket.puts message
     end
 
     def post_message channel, message
@@ -46,13 +53,6 @@ module Irc2Murmur
         user = nick_from_message message
         msg = msg_from_message message
         notify_observers user, msg
-      end
-    end
-
-    def run
-      until @socket.eof? do
-        msg = @socket.gets
-        process msg
       end
     end
 
