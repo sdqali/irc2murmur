37450a85653dff05a40619f8d316c069adef83a8
Sadique Ali <sadiqalikm@gmail.com>
2011-12-04 15:43:41 +0530

    Fixed irc_observer specs

--- spec/irc_observer_spec.rb
+++ spec/irc_observer_spec.rb
@@ -1,20 +1,21 @@
 require File.dirname(__FILE__) + '/../lib/irc_observer'
-require File.dirname(__FILE__)+'/spec_helper'
 
 include Irc2Murmur
 describe IrcObserver do
   it "should add itself as observer to the bot" do
-    bot = StubBot.new
+    bot = mock
     observer = IrcObserver.new nil
     bot.should_receive(:add_observer).with(observer)
     observer.register bot
   end
 
   it "should ask Mingle to post a murmur on being notified" do
-    bot = StubBot.new
+    bot = mock
     mingle = mock
+    bot.stub!(:add_observer)
     observer = IrcObserver.new mingle
     observer.register bot
+
     mingle.should_receive(:post_murmur).with("foo said: bar")
     observer.update "foo", "bar"
   end
